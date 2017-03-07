edf77c39aa5e4fa1adf87b39671ae45c352de791
nill-zhang <andy.ledway3@gmail.com>
2017-03-07 12:28:41 -0500

    adding comments for Strings.Join

--- Strings.Join().go
+++ Strings.Join().go
@@ -7,6 +7,7 @@ func Join(a []string, sep string) string {
 	if len(a) == 1 {
 		return a[0]
 	}
+        // n is the length of the underlying array that will hold the result string
 	// total len(a)-1 seps are needed to fill out the gaps
 	n := len(sep) * (len(a) - 1)
         // plus total length of all the elements
@@ -14,12 +15,12 @@ func Join(a []string, sep string) string {
 		n += len(a[i])
 	}
 
-        // allocate n byte slice to hold the result string
+        // allocate n-byte long slice to store the result string
 	b := make([]byte, n)
 
         // copy the first slice element to the result bytes
-        // the rest will copy as sep-element pair
-        // bp is the starting cursor for the remaining space
+        // the rest will be copied as sep-element pair
+        // bp is the starting cursor for the remaining available space
 	bp := copy(b, a[0])
 	for _, s := range a[1:] {
 		bp += copy(b[bp:], sep)
